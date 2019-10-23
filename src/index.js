aa653c3b0c0717b275cf10ecb339d5e02c2fd913
mwoodiwiss <malachaiwoodiwiss1999@gmail.com>
2019-10-22 19:28:10 -0600

    [A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import Reducer from './reducers/Reducer';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(Reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+  <App/>
+</Provider>,
+rootElement);
